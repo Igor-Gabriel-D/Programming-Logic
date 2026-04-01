877d1449fb0a7e75d8d2867e441938a04e5d89b0
Igor-Gabriel-D <igordantas.dev@gmail.com>
2026-04-01 12:55:23 +0000

    feat: add solved exercise 001 (book 01)

---
+++ exercices/book_01/solved_exercises/ex001.py
@@ -0,0 +1,11 @@
+# Write an algorithm that receives four integers, calculates and displays their sum
+
+n1 = int(input("First number: "))
+n2 = int(input("Second number: "))
+n3 = int(input("Third number: "))
+n4 = int(input("Fourth number:"))
+
+sum_ = n1 + n2 +n3 + n4
+
+print("Sum:", sum_)
+
