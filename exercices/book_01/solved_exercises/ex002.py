3a25b271eb1ff05c43c19cbfa3152aff3fd00cf0
Igor-Gabriel-D <igordantas.dev@gmail.com>
2026-04-01 13:05:04 +0000

    feat: add solved exercise 002 (book 01)

---
+++ exercices/book_01/solved_exercises/ex002.py
@@ -0,0 +1,9 @@
+# Write a program that receives three grades, calculates and displays the arithmetic mean.
+
+grade1 = float(input("First grade: "))
+grade2 = float(input("Second grade: "))
+grade3 = float(input("Third grade: "))
+
+mean = (grade1 + grade2 + grade3) / 3
+
+print("Arithmetic mean:",mean)
