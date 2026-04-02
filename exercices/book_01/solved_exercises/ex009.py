520d12118d6ca867dfedd11feab230472f973d1b
Igor-Gabriel-D <igordantas.dev@gmail.com>
2026-04-02 11:58:48 +0000

    feat: add solved exercise 009 (book 01)

---
+++ exercices/book_01/solved_exercises/ex009.py
@@ -0,0 +1,6 @@
+# Write a program that calculates and displays the area of a triangle, knowing that: area = (base * height)/2
+
+base = float(input("Base: "))
+height = float(input("Height: "))
+
+print(f"Area of triangle: {(base * height)/2}")
