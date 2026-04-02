7127832dfc084f24a02ca3143fb9a8960d2c8f21
Igor-Gabriel-D <igordantas.dev@gmail.com>
2026-04-02 13:36:15 +0000

    feat: add solved exercise 010 (book 01)

---
+++ exercices/book_01/solved_exercises/ex010.py
@@ -0,0 +1,8 @@
+# Write a program that calculates and displays the area of a circle, knowing that: area = pi * R^2
+
+import math
+
+radius = float(input("Radius: "))
+
+print(f"Area: {math.pi * radius*radius}")
+
